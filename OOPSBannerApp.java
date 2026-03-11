c85b4db462201399f1a0108f0a91af5b9beaf8af
YUVAN9798 <ys6113@srmist.edu.in>
2026-03-11 09:49:15 +0530

    UC8: Implement HashMap for Character pattern lookup

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,57 +1,62 @@
-class OOPSBannerApp {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
 
-        String[] banner = {
-                String.join("   ", getO(0), getO(0), getP(0), getS(0)),
-                String.join("   ", getO(1), getO(1), getP(1), getS(1)),
-                String.join("   ", getO(2), getO(2), getP(2), getS(2)),
-                String.join("   ", getO(3), getO(3), getP(3), getS(3)),
-                String.join("   ", getO(4), getO(4), getP(4), getS(4)),
-                String.join("   ", getO(5), getO(5), getP(5), getS(5)),
-                String.join("   ", getO(6), getO(6), getP(6), getS(6))
-        };
+class OOPsBannerApp {
 
-        for (String line : banner) {
+    /**
+     * Builds and returns character pattern map
+     */
+    public static Map<Character, String[]> buildPatternMap() {
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+
+    /**
+     * Displays banner message
+     */
+    public static void displayBanner(String message, Map<Character, String[]> map) {
+        for (int i = 0; i < 7; i++) {
+            StringBuilder line = new StringBuilder();
+            for (char ch : message.toCharArray()) {
+                line.append(map.get(ch)[i]).append(" ");
+            }
             System.out.println(line);
         }
     }
 
-    static String getO(int row) {
-        String[] o = {
-                "  ***** ",
-                " *     *",
-                " *     *",
-                " *     *",
-                " *     *",
-                " *     *",
-                "  ***** "
-        };
-        return o[row];
-    }
-
-    static String getP(int row) {
-        String[] p = {
-                "  ***** ",
-                " *     *",
-                " *     *",
-                " ***** ",
-                " *      ",
-                " *      ",
-                " *      "
-        };
-        return p[row];
-    }
-
-    static String getS(int row) {
-        String[] s = {
-                "  ***** ",
-                " *      ",
-                " *      ",
-                " ***** ",
-                "        *",
-                "        *",
-                " ***** "
-        };
-        return s[row];
+    public static void main(String[] args) {
+        Map<Character, String[]> patternMap = buildPatternMap();
+        displayBanner("OOPS", patternMap);
     }
 }
